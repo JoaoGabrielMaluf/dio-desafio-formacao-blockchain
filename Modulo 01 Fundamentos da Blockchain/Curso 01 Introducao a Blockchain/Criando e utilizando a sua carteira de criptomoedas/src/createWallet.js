0231f25bffe9dde0b0a4676bc5045f227923fbea
João Gabriel Maluf <joaogabrielmaluf@gmail.com>
2024-08-19 02:13:07 -0300

    feat: add .gitignore

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -1,33 +1,39 @@
-//importando as dependencias
+// Importando as dependencias
 const bip32 = require('bip32')
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
-//definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
+// Definir a rede
+// bitcoin.networks.bitcoin => Rede Principal 
+// bitcoin.networks.testnet => Rede de Teste 
 const network = bitcoin.networks.testnet
 
-//derivação de carteiras HD
-const path = `m/49'/1'/0'/0` 
+// Derivação de endereço de carteiras HD
+// `m/49'/0'/0'/0` => Principal 
+// `m/49'/1'/0'/0` => Teste
+const path = `m/49'/1'/0'/0`
 
-//criando o mnemonic para a seed (palavras de senha)
+// Criando mnemonic para a seed (palavras de senha)
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-//criando a raiz da cartiera HD
-let root = bip32.fromSeed(seed, network)
+// Criando a raíz da carteira HD
+let root = bip32.fromSeed(seed,network)
 
-//criando uma conta - par pvt-pub keys
+// Criando uma conta - par de chaves PVT e PUB keys
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
 let btcAddress = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
-    network: network,
+    network: network
 }).address
 
-console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("################################")
+console.log("CARTEIRA GERADA")
+console.log("--------------------------------")
+console.log("Endereço: ",btcAddress)
+console.log("Chave Privada: ",node.toWIF())
+console.log("Seed: ",mnemonic)
+console.log("################################")
+
